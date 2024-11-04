92131f58b7302af0e28960914f709ed05dbbd12b
AkintolaDavid <akintoladvaid66@gmail.com>
2024-11-04 14:25:05 +0100

    first

--- routes/upload.js
+++ routes/upload.js
@@ -4,7 +4,7 @@ const path = require("path");
 // Set storage for files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Specify the upload folder
+    cb(null, "uploads"); // Specify the upload folder
   },
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to file name
