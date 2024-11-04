98639277145a9dd40871ac814385c10ea291d1e3
AkintolaDavid <akintoladvaid66@gmail.com>
2024-11-04 14:42:22 +0100

    first

--- routes/upload.js
+++ routes/upload.js
@@ -1,17 +1,17 @@
-const multer = require("multer");
-const path = require("path");
+// const multer = require("multer");
+// const path = require("path");
 
-// Set storage for files
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads"); // Specify the upload folder
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to file name
-  },
-});
+// // Set storage for files
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads"); // Specify the upload folder
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + path.extname(file.originalname)); // Append timestamp to file name
+//   },
+// });
 
-// Initialize upload with a file size limit if necessary
-const upload = multer({ storage });
+// // Initialize upload with a file size limit if necessary
+// const upload = multer({ storage });
 
-module.exports = upload;
+// module.exports = upload;
