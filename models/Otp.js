3e6c2e01a92dbe53ee1622cd4e75c5c4becd43be
AkintolaDavid <akintoladvaid66@gmail.com>
2024-11-14 11:25:20 +0100

    first

--- models/Otp.js
+++ models/Otp.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-// Assuming you have a schema for OTP
-const otpSchema = new mongoose.Schema({
-  email: String,
-  otp: Number,
-  expiresAt: Date,
-});
 
-// Create a TTL index on the expiresAt field
-otpSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 0 });
+const otpSchema = new mongoose.Schema({
+  email: { type: String, required: true },
+  otp: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now, expires: 300 }, // Expires in 5 minutes (300 seconds)
+});
 
 module.exports = mongoose.model("Otp", otpSchema);
